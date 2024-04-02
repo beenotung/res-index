a94080de88e0a3f0b45d9113897aecabfd415bba
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-04-02 21:04:58 +0800

    feat: make basic search UI for repo list

--- server/app/pages/home.tsx
+++ server/app/pages/home.tsx
@@ -1,6 +1,14 @@
 import { o } from '../jsx/jsx.js'
 import { prerender } from '../jsx/html.js'
 import SourceCode from '../components/source-code.js'
+import { mapArray } from '../components/fragment.js'
+import { proxy } from '../../../db/proxy.js'
+import { DynamicContext } from '../context.js'
+import Style from '../components/style.js'
+import { db } from '../../../db/db.js'
+import { Script } from '../components/script.js'
+import { VElement } from '../../../client/jsx/types.js'
+import { EarlyTerminate } from '../helpers.js'
 
 // Calling <Component/> will transform the JSX into AST for each rendering.
 // You can reuse a pre-compute AST like `let component = <Component/>`.
@@ -8,30 +16,79 @@ import SourceCode from '../components/source-code.js'
 // If the expression is static (not depending on the render Context),
 // you don't have to wrap it by a function at all.
 
+let style = Style(/* css */ `
+.list {
+  padding: 0.25rem;
+}
+.repo {
+  padding: 0.25rem;
+}
+`)
+
+let script = Script(/* javascript */ `
+searchForm.keyword.focus()
+`)
+
 let content = (
   <div id="home">
-    <h1>Home Page</h1>
-    <p>You can get started by replacing the contents in this page</p>
-    <p>
-      When the browser load this url, the server responses complete html content
-      to the GET request. This allows the browser to perform meaningful paint as
-      soon as possible. And it's ideal for SEO.
-    </p>
-    <p>
-      Try some reactive demo:{' '}
-      <a href="https://liveviews.cc/thermostat" target="_blank">
-        Thermostat
-      </a>
-      ,{' '}
-      <a href="https://liveviews.cc/form" target="_blank">
-        Form Demo
-      </a>
-    </p>
+    {style}
+    <h1>FOSS Git Repository Index</h1>
+    <Page />
+    {script}
     <SourceCode page="home.tsx" />
   </div>
 )
 
-// And it can be pre-rendered into html as well
-let Home = prerender(content)
+let select_repo = db
+  .prepare(
+    /* sql */ `
+select id from repo
+where name like :keyword
+`,
+  )
+  .pluck()
 
-export default Home
+function Page(attrs: {}, context: DynamicContext) {
+  let params = new URLSearchParams(context.routerMatch?.search)
+  let keyword = params.get('keyword') || ''
+  let repos = select_repo
+    .all({ keyword: '%' + keyword + '%' })
+    .map((id: any) => proxy.repo[id])
+  let result: VElement = [
+    'div#result',
+    {},
+    [
+      <p>{repos.length} matches</p>,
+      <div class="list">
+        {mapArray(repos, repo => (
+          <div class="repo">
+            <div>
+              <b>{repo.name}</b> <sub>by {repo.author!.username}</sub>
+            </div>
+            <a target="_blank" href={repo.url}>
+              {repo.url}
+            </a>
+          </div>
+        ))}
+      </div>,
+    ],
+  ]
+  if (context.type == 'ws') {
+    context.ws.send(['update', result])
+    throw EarlyTerminate
+  }
+  return (
+    <form onsubmit="emitForm(event)" id="searchForm">
+      <label>
+        Keyword: <input name="keyword" value={keyword} />
+      </label>{' '}
+      <input type="submit" value="Search" />
+      {result}
+    </form>
+  )
+}
+
+// And it can be pre-rendered into html as well
+// let Home = prerender(content)
+
+export default content
