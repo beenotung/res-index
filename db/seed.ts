a47bc12eaa7226dbfe5356afb4d7ff27aa12d169
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-04-02 21:43:49 +0800

    db: add more sample repos - scan multiple git repo host directories - auto checkout if the repo is empty

--- db/seed.ts
+++ db/seed.ts
@@ -2,54 +2,94 @@ import { find, seedRow } from 'better-sqlite3-proxy'
 import { proxy } from './proxy'
 import { db } from './db'
 import { cleanRepoUrl, parseRepoUrl } from './format'
-import { readdirSync } from 'fs'
+import { readdirSync, statSync } from 'fs'
 import { join } from 'path'
 import { env } from 'process'
 import { homedir } from 'os'
+import { execSync } from 'child_process'
 
 // This file serve like the knex seed file.
 //
 // You can setup the database with initial config and sample data via the db proxy.
 
 function seed_sample_data() {
-  proxy.author[1] = { username: 'beenotung' }
+  function reset() {
+    db.exec('delete from repo')
+    db.exec('delete from page')
+    db.exec('delete from author')
+    db.exec('delete from programming_language')
+  }
+  // reset()
 
-  proxy.programming_language[1] = { name: 'typescript' }
+  function getLanguageId(name: string): number {
+    return (
+      find(proxy.programming_language, { name })?.id ||
+      proxy.programming_language.push({ name })
+    )
+  }
+
+  function detectLanguage(dir: string): number | null {
+    if (dir.endsWith('DefinitelyTyped')) return getLanguageId('Typescript')
+    let filenames = readdirSync(dir)
+    if (filenames.length == 1 && filenames[0] === '.git') {
+      try {
+        console.log('checkout:', dir)
+        execSync('git checkout master', { cwd: dir })
+      } catch (error) {
+        // branch not called master?
+      }
+      filenames = readdirSync(dir)
+    }
+    if (filenames.includes('tsconfig.json')) return getLanguageId('Typescript')
+    if (filenames.includes('package.json')) return getLanguageId('Javascript')
+    if (filenames.some(filename => filename.endsWith('.html')))
+      return getLanguageId('HTML')
+    return null
+  }
 
   let repo_id = 0
-  function seedRepo(name: string) {
-    repo_id++
-    proxy.page[repo_id] = {
-      url: 'https://github.com/beenotung/' + name,
-      payload: null,
-      check_time: null,
-      update_time: null,
-    }
-    proxy.repo[repo_id] = {
-      author_id: 1,
-      name: name,
-      is_fork: false,
-      url: 'https://github.com/beenotung/' + name,
-      desc: 'stub',
-      programming_language_id: 1,
-      website: null,
-      stars: null,
-      watchers: null,
-      forks: null,
-      readme: null,
-      last_commit: null,
-      page_id: repo_id,
-    }
-  }
 
-  let names = readdirSync(
-    join(homedir(), 'workspace', 'github.com', 'beenotung'),
-  )
-  for (let name of names) {
-    seedRepo(name)
+  let hosts = ['github.com', 'gitlab.com', 'bitbucket.org']
+  for (let host of hosts) {
+    let host_dir = join(homedir(), 'workspace', host)
+    let usernames = readdirSync(host_dir)
+    for (let username of usernames) {
+      let author_id =
+        find(proxy.author, { username })?.id || proxy.author.push({ username })
+
+      let user_dir = join(host_dir, username)
+      let repos = readdirSync(user_dir)
+      for (let repo of repos) {
+        let repo_dir = join(user_dir, repo)
+        if (!statSync(repo_dir).isDirectory()) continue
+        repo_id++
+        let url = `https://${host}/${username}/${repo}`
+        proxy.page[repo_id] = {
+          url,
+          payload: null,
+          check_time: null,
+          update_time: null,
+        }
+        proxy.repo[repo_id] = {
+          author_id,
+          name: repo,
+          is_fork: false,
+          url,
+          desc: 'stub',
+          programming_language_id: detectLanguage(repo_dir),
+          website: null,
+          stars: null,
+          watchers: null,
+          forks: null,
+          readme: null,
+          last_commit: null,
+          page_id: repo_id,
+        }
+      }
+    }
   }
 }
-if (proxy.repo.length == 0) {
+if ('dev' || proxy.repo.length == 0) {
   seed_sample_data()
 }
 
