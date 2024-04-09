450e00d3b27f8dcb1d306e5dd9c31b201ce05077
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-04-09 12:12:41 +0800

    chore: show file size when running scripts/export-db.sh

--- scripts/export-db.sh
+++ scripts/export-db.sh
@@ -3,8 +3,10 @@ set -e
 set -o pipefail
 
 cd data
+du -sh db.sqlite3
 sqlite3 db.sqlite3 '.backup slim.sqlite3'
 sqlite3 slim.sqlite3 'update page set payload = null'
 sqlite3 slim.sqlite3 '.dump' > slim.sql
+du -sh slim.sql
 cat slim.sql | xz - > db.sql.xz
-ls -lh db.sql.xz
+du -sh db.sql.xz
