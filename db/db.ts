063cc1f263c3836cb38ab8a310387c59da1c2285
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-09-23 00:55:55 +0800

    chore: tune database to use more ram for caching

--- db/db.ts
+++ db/db.ts
@@ -18,4 +18,15 @@ export let db: DBInstance = newDB({
   migrate: false,
   fileMustExist: true,
   WAL: true,
+  synchronous: 'NORMAL',
 })
+
+// Set a higher threshold for auto-checkpoints to reduce frequency of flushing from WAL to main file
+db.pragma('wal_autocheckpoint = 1000')
+
+// try around 100MB to 200MB
+// 250MB cache
+db.pragma('cache_size = -250000')
+
+// use memory instead of disk for temporary table during join table
+db.pragma('temp_store = MEMORY')
